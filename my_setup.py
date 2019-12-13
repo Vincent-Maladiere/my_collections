7665c3a7a80bcf567c27684c01b14473fe4d680e
Vincent Maladiere <maladiere.vincent@yahoo.fr>
2019-12-13 19:07:40 +0100

    first commit

---
+++ my_setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+
+setup(name="my_collections", 
+    version="1.0.0", 
+    url="None",
+    maintainer='Vincent Maladiere',
+    maintainer_email='maladiere.vincent@gmail.com',
+    py_modules=['my_collections'],
+    description="experimental collections module.") 
